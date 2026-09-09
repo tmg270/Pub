2db8c43d64261cb736dc94af69db2502579e283f
tmg270 <thatmidnightgambler@gmail.com>
2026-09-09 12:02:19 +0100

    Delete razor-cuda-2.4 directory

--- razor-cuda-2.4/enable-huge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem Grants Lock pages in memory for 2 MiB pages. Run as Administrator once, then SIGN OUT.
-razor.exe --enable-huge
-pause
--- razor-cuda-2.4/mine-5950.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem 5950X: 32 threads (that's your 16 kH/s) + RTX 3080 RandomX lane
-razor.exe --threads 32 --no-tune --backend jit-full --prefetch t0 --cuda --pool pool.shear.digital:1111 --user she1q8qs265j8wsfhrtgtufgdfn780q377x65zawsz9.win5950
-pause
